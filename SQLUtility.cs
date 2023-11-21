d18719accaae1e0b2e7b6acebfa8992c6db3264f
zissysilber <zissysilber@gmail.com>
2023-11-21 11:50:33 -0500

    initial commit

---
+++ SQLUtility.cs
@@ -0,0 +1,26 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CPUFramework
+{
+    public class SQLUtility
+    {
+        public static string ConnectionString = "";
+        public static DataTable GetDataTable(string sqlstatement) // - take a SQL statement and return a DataTable
+        {
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
