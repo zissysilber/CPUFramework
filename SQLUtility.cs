210de5c7147d7f4cbb6699d8788af44623e58799
zissysilber <zissysilber@gmail.com>
2024-01-17 22:24:43 -0500

    complete recipe app with login and security

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -7,8 +7,27 @@ namespace CPUFramework
 {
     public class SQLUtility
     {
-        public static string ConnectionString = "";
+        private static string ConnectionString = "";
 
+        public static void SetConnectionString(string connstring, bool tryopen, string userid = "", string password="")
+        {
+            ConnectionString = connstring;
+            if(userid != "")
+            {
+                SqlConnectionStringBuilder b = new();
+                b.ConnectionString = ConnectionString;
+                b.UserID = userid;
+                b.Password = password;
+                ConnectionString = b.ConnectionString;
+            }
+            if (tryopen)
+            {
+                using(SqlConnection conn = new SqlConnection(ConnectionString))
+                {
+                    conn.Open();
+                }
+            }
+        }
         public static SqlCommand GetSqlCommand(string sprocname)
         {
             SqlCommand cmd;
@@ -315,6 +334,7 @@ namespace CPUFramework
             StringBuilder sb = new StringBuilder();
             if (cmd.Connection != null)
             {
+                //sb.AppendLine($"--{cmd.Connection.ConnectionString}");
                 sb.AppendLine($"--{cmd.Connection.DataSource}");
                 sb.AppendLine($"use {cmd.Connection.Database}");
                 sb.AppendLine("go");
