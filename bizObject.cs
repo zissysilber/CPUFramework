7ce94561acd518ef27b9ad978ea1fce0809f0425
zissysilber <zissysilber@gmail.com>
2024-05-28 11:54:23 -0400

    refactored

--- bizObject.cs
+++ bizObject.cs
@@ -5,22 +5,33 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace CPUFramework
 {
-    public class bizObject
+    public class bizObject : INotifyPropertyChanged
     {
         string _tablename = ""; string _getsproc = ""; string _updatesproc = ""; string _deletesproc = "";
         string _primarykeyname = ""; string _primarykeyparamname = "";
         DataTable _datatable = new();
-        public bizObject(string tablename)
+        List<PropertyInfo> _properties = new();
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+        public bizObject()
         {
-            _tablename = tablename;
-            _getsproc = tablename + "Get";
-            _updatesproc = tablename + "Update";
-            _deletesproc = tablename + "Delete"; 
-            _primarykeyname = tablename + "Id";
+            Type t = this.GetType();
+            _tablename = t.Name;
+            if (_tablename.ToLower().StartsWith("biz")) { _tablename = _tablename.Substring(3); }
+            _getsproc = _tablename + "Get";
+            _updatesproc = _tablename + "Update";
+            _deletesproc = _tablename + "Delete";
+            _primarykeyname = _tablename + "Id";
             _primarykeyparamname = "@" + _primarykeyname;
+            _properties = t.GetProperties().ToList<PropertyInfo>();
 
         }
         public DataTable Load(int primarykeyvalue)
@@ -29,10 +40,26 @@ namespace CPUFramework
             SqlCommand cmd = SQLUtility.GetSqlCommand(_getsproc);
             SQLUtility.SetParamValue(cmd, _primarykeyparamname, primarykeyvalue);
             dt = SQLUtility.GetDataTable(cmd);
+            if (dt.Rows.Count > 0)
+            {
+                LoadProps(dt.Rows[0]);
+            }
+
             _datatable = dt;
             return dt;
         }
 
+        private void LoadProps(DataRow dr)
+        {
+            foreach (DataColumn col in dr.Table.Columns)
+            {
+                var prop = GetProp(col.ColumnName, false, true);
+                if (prop != null)
+                {
+                    prop.SetValue(this, dr[col.ColumnName]);
+                }
+            }
+        }
         public void Delete(DataTable datatable)
         {
             int id = (int)datatable.Rows[0][_primarykeyname];
@@ -42,7 +69,32 @@ namespace CPUFramework
 
         }
 
-        public  void Save(DataTable datatable)
+        public void Save()
+        {
+            SqlCommand cmd = SQLUtility.GetSqlCommand(_updatesproc);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                var prop = GetProp(param.ParameterName, true, false);
+                if (prop != null)
+                {
+                    param.Value = prop.GetValue(this);
+                }
+            }
+            SQLUtility.ExecuteSQL(cmd);
+            foreach (SqlParameter param in cmd.Parameters)
+            {
+                if (param.Direction == ParameterDirection.InputOutput)
+                {
+                    var prop = GetProp(param.ParameterName, false, true);
+                        if (prop != null)
+                    {
+                        prop.SetValue(this, param.Value);
+                    }
+                }
+            }
+        }
+
+        public void Save(DataTable datatable)
         {
             if (datatable.Rows.Count == 0)
             {
@@ -52,6 +104,22 @@ namespace CPUFramework
             DataRow r = datatable.Rows[0];
             SQLUtility.SaveDataRow(r, _updatesproc);
 
+        }
+        private PropertyInfo? GetProp(string propname, bool forread, bool forwrite)
+        {
+            propname = propname.ToLower();
+            if (propname.StartsWith("@")) { propname.Substring(1); }
+            PropertyInfo? prop = _properties.FirstOrDefault(p =>
+            p.Name.ToLower() == propname
+            && (forread == false || p.CanRead == true)
+            && (forwrite == false || p.CanWrite == true)
+            );
+            return prop;
+        }
+
+        protected void InvokePropertyChanged([CallerMemberName] string propertyname = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
         }
     }
 }
